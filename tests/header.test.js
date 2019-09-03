636852d2f5f1a5fa917efd5b9a62a6a9f26e29b6
clvrbarros <cleversonferreirab@gmail.com>
2019-09-03 20:54:12 -0300

    add test with puppeteer

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,17 +1,26 @@
 const puppeteer = require('puppeteer');
 
-test('Add two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
+let broswer, page;
+
+beforeEach(async () => {
+  broswer = await puppeteer.launch({
+    headless: false
+  });
+  page = await broswer.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await broswer.close();
 });
 
 test('We can launch a broswer', async () => {
-  const broswer = await puppeteer.launch({
-    headless: false
-  });
-  const page = await broswer.newPage();
-  await page.goto('localhost:3000');
-
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
